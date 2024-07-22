b5ae9ce02244aa36d5f8ae0eed78efa09fc0a023
Tantantan-py <exactbrycetan@gmail.com>
2024-07-22 17:35:19 -0500

    update gradient dark effect

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -11,13 +11,14 @@ const HomeCoverSection = ({ blogs }) => {
   return (
     <div className="w-full inline-block">
       <article className="flex flex-col items-start justify-end mx-10 relative h-[85vh]">
+        <div className="absolute h-full top-0 left-0 buttom-0 right-0 bg-gradient-to-b from-transparent from-0% to-black rounded-3xl z-0" />
         <Image
           src={blog.image.filePath.replace("../public", "")}
           placeholder="blur"
           blurDataURL={blog.image.blurhashDataUrl}
           alt={blog.title}
           fill
-          className="w-full h-full object-center object-cover rounded-3xl"
+          className="w-full h-full object-center object-cover rounded-3xl -z-10"
         />
       </article>
     </div>
