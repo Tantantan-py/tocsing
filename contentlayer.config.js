145bdf5e61159813a744c786debc3794371c42ef
Tantantan-py <exactbrycetan@gmail.com>
2024-07-22 13:49:35 -0500

    generate contentlayer

---
+++ contentlayer.config.js
@@ -0,0 +1,55 @@
+// contentlayer.config.js
+
+import { makeSource, defineDocumentType } from "@contentlayer/source-files";
+
+const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: "**/**/*.mdx",
+  fields: {
+    title: {
+      type: "string",
+      require: true,
+    },
+    publishAt: {
+      type: "date",
+      require: true,
+    },
+    updateAt: {
+      type: "date",
+      require: true,
+    },
+    description: {
+      type: "string",
+      require: true,
+    },
+    isPublished: {
+      type: "boolean",
+      default: true,
+    },
+    author: {
+      type: "string", // or make it to a list
+      require: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+    image: {
+      type: "string",
+      require: true,
+    },
+    // For the Image needs to update the store method
+  },
+  computedFields: {
+    url_path: {
+      type: "string",
+      resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  /* options */
+  contentDirPath: "content",
+  documentTypes: [Blog],
+});
