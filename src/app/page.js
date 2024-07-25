535aff211eeb8c6292ebe40f52ee3d53d5f4b04b
Tantantan-py <exactbrycetan@gmail.com>
2024-07-25 17:57:03 -0500

    updates on BlogLayoutThree

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import HomeCoverSection from "@/components/Home/HomeCoverSection";
 import FeaturedPosts from "@/components/Home/FeaturedPosts";
-import {allBlogs} from "contentlayer/generated";
+import RecentPosts from "@/components/Home/RecentPosts";
+import { allBlogs } from "contentlayer/generated";
 
 export default function Home() {
-  
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
     </main>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ const Header = () => {
       <Logo />
       <nav
         className="w-max py-3 px-8 border border-solid rounded-full font-medium capitalize flex items-center
-      fixed top-6 right-1/2 translate-x-1/2 bg-white/80 backdrop-blur-sm"
+      fixed top-6 right-1/2 translate-x-1/2 bg-white/80 backdrop-blur-sm z-50"
       >
         <Link href="/" className="mr-2">
           Home
@@ -17,7 +17,7 @@ const Header = () => {
         <Link href="/about" className="mx-2">
           About
         </Link>
-        <Link href="/contact" className="mr-2">
+        <Link href="/contact" className="mx-2">
           Contact
         </Link>
         <button>
@@ -29,13 +29,13 @@ const Header = () => {
           href="https://www.linkedin.com/in/lucian-tan/"
           className="inline-block w-6 h-6 mr-4"
         >
-          <LinkedInIcon className="hover:scale-125 transition-all ease duration-200"/>
+          <LinkedInIcon className="hover:scale-125 transition-all ease duration-200" />
         </a>
         <a
           href="https://github.com/Tantantan-py/tocsing"
           className="inline-block w-6 h-6 mr-4"
         >
-          <GitHubIcon className="hover:scale-125 transition-all ease duration-200"/>
+          <GitHubIcon className="hover:scale-125 transition-all ease duration-200" />
         </a>
       </div>
     </header>
