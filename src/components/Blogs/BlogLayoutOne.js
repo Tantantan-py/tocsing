8a994aa67250c8c14c09c53b4a07820957a0cbb1
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2024-07-24 23:40:23 -0500

    update featured blogs layouts

--- src/components/Blogs/BlogLayoutOne.js
+++ src/components/Blogs/BlogLayoutOne.js
@@ -2,11 +2,16 @@ import React from "react";
 import Tag from "../Elements/Tag";
 import Link from "next/link";
 import Image from "next/image";
+import { slug } from "github-slugger";
 
-const BlogLayOutOne = ({ blog }) => {
+const BlogLayoutOne = ({ blog }) => {
   return (
-    <div className="inline-block ovreflow-hidden rounded-xl">
-      <div className="absolute h-full top-0 left-0 buttom-0 right-0 bg-gradient-to-b from-transparent from-0% to-black/80 rounded-3xl z-10" />
+    <div className="group inline-block overflow-hidden rounded-xl">
+      <div
+        className="absolute top-0 left-0 bottom-0 right-0 h-full
+            bg-gradient-to-b from-transparent from-0% to-black/90 rounded-xl z-10
+            "
+      />
       <Image
         src={blog.image.filePath.replace("../public", "")}
         placeholder="blur"
@@ -14,17 +19,22 @@ const BlogLayOutOne = ({ blog }) => {
         alt={blog.title}
         width={blog.image.width}
         height={blog.image.height}
-        className="w-full h-full object-center object-cover rounded-xl"
+        className="w-full h-full object-center object-cover rounded-xl group-hover:scale-105 transition-all ease duration-300"
+        sizes="(max-width: 1180px) 100vw, 50vw"
       />
-      <div className="w-full absolute bottom-0 p-10 z-20">
+
+      <div className="w-full absolute bottom-0 p-4 xs:p-6 sm:p-10 z-20">
         <Tag
-          link={`/categories/${blog.tags[0]}`}
+          link={`/categories/${slug(blog.tags[0])}`}
           name={blog.tags[0]}
-          className="px-6 text-sm py-2 !border"
+          className="px-6 text-xs  sm:text-sm py-1 sm:py-2 !border "
         />
         <Link href={blog.url_path} className="mt-6">
-          <h2 className="font-bold capitalize text-2xl text-white">
-            <span className="bg-gradient-to-r from-white to-grey bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">
+          <h2 className="font-bold capitalize text-sm xs:text-base sm:text-xl md:text-2xl text-white mt-2 sm:mt-4">
+            <span
+              className="bg-gradient-to-r from-purple-600 to-purple-400 bg-[length:0px_6px]
+                group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 "
+            >
               {blog.title}
             </span>
           </h2>
@@ -34,4 +44,4 @@ const BlogLayOutOne = ({ blog }) => {
   );
 };
 
-export default BlogLayOutOne;
+export default BlogLayoutOne;
--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -12,13 +12,13 @@ const FeaturedPosts = ({ blogs }) => {
       </h2>
       <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-16">
         <article className="col-span-1 row-span-2 relative">
-          <BlogLayOutOne blog={sortedBlogs[5]} />
+          <BlogLayOutOne blog={sortedBlogs[1]} />
+        </article>
+        <article className="col-span-1 row-span-1 relative">
+          <BlogLayoutTwo blog={sortedBlogs[2]} />
         </article>
         <article className="col-span-1 row-span-1 relative">
           <BlogLayoutTwo blog={sortedBlogs[3]} />
-        </article>
-        <article className="col-span-1 row-span-1 relative">
-          <BlogLayoutTwo blog={sortedBlogs[4]} />
         </article>
       </div>
     </section>
