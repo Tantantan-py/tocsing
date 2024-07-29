fd4003a17c13e3319cc0e1fccbeaa55bd21fff0f
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2024-07-29 01:04:25 -0500

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,56 @@
+"use client";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { GitHubIcon, LinkedInIcon } from "../Header/Icons";
 
 const Footer = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
-    <footer>
-      <h3>Interesting Stories | Updates | Guides</h3>
-      <p>
+    <footer className="mt-16 rounded-2xl bg-dark m-10 flex flex-col items-center text-light">
+      <h3 className="mt-16 font-medium text-center capitalize text-4xl px-4">
+        Interesting Stories | Updates | Guides
+      </h3>
+      <p className="mt-5 px-4 text-cent w-3/4 font-light text-base">
         Subscribe to learn about new technology and updates. Join over 5000+
         members community to stay up to date with latest news.
       </p>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-6 min-w-[384px] flex items-stretch bg-light p-2 rounded mx04"
+      >
+        <input
+          type="email"
+          placeholder="Enter Your Email and Join Us!"
+          {...register}
+          className="w-full bg-transparent text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
+        />
+
+        <input
+          type="submit"
+          className="bg-dark text-light cursor-pointer font-medium rounded px-5 py-1"
+        />
+      </form>
+      <div className="flex items-center mt-8">
+        <a
+          href="https://www.linkedin.com/in/lucian-tan/"
+          className="inline-block w-6 h-6 mr-4"
+        >
+          <LinkedInIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a
+          href="https://github.com/Tantantan-py/tocsing"
+          className="inline-block w-6 h-6 mr-4 fill-light"
+        >
+          <GitHubIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+      </div>
     </footer>
   );
 };
