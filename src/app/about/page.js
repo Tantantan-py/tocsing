e1f800c7c4a4d67bac6fdb6427cf533767493798
Tantantan-py <exactbrycetan@gmail.com>
2024-08-01 15:25:40 -0500

    Update About page rest part and start to update Contact parts

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,19 @@
 import AboutCoverSection from "@/components/About/AboutCoverSection";
 import Skills from "@/components/About/Skills";
+import Link from "next/link";
 
 export default function About() {
   return (
     <>
       <AboutCoverSection />
       <Skills />
+      <h2 className="m-8 font-semibold text-2xl self-start mx-20 text-dark">
+        Have any suggestions? Please reach out to our team{" "}
+        <Link href="/contact" className="!underline underline-offset-2">
+          here
+        </Link>
+        !
+      </h2>
     </>
   );
 }
