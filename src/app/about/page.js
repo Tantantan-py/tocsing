f447fcdba706e7c5ca72e8124a72a521b1e26e8b
Tantantan-py <exactbrycetan@gmail.com>
2024-08-01 14:04:16 -0500

    update about page skills list

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,11 @@
 import AboutCoverSection from "@/components/About/AboutCoverSection";
+import Skills from "@/components/About/Skills";
 
 export default function About() {
   return (
     <>
       <AboutCoverSection />
+      <Skills />
     </>
   );
 }
