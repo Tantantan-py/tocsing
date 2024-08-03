d93f86c2461bf9873b8b9339d4b1852f69fa0fd6
Xu Tan(Lucian) <tan.xu1@northeastern.edu>
2024-08-03 18:59:00 -0500

    try to update dotlottie

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,11 @@
+import LottieAnimation from "@/components/Contact/LottieAnimation";
+
 export default function Contact() {
   return (
     <section className="w-full h-[75vh] border-b-2 border-solid border-dark flex flex-row items-center justify-center text-dark">
-      <div>lottie animation</div>
+      <div>
+        <LottieAnimation />
+      </div>
       <div>contact form</div>
     </section>
   );
---
+++ src/components/Contact/LottieAnimation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const LottieAnimation = () => {
+  return <DotLottieReact src="/animation_llqd7ey4.lottie" loop autoplay />;
+};
+
+export default LottieAnimation;
