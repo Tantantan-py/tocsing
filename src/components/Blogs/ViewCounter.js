d2d7212942e9ba18ce542b869e8e2907ee235a2a
Tantantan-py <exactbrycetan@gmail.com>
2024-08-07 17:02:06 -0500

    update view counter, connecting with supabase

---
+++ src/components/Blogs/ViewCounter.js
@@ -0,0 +1,34 @@
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import React, { useEffect, useState } from "react";
+
+const supabase = createClientComponentClient();
+
+const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
+  const [views, setViews] = useState(0);
+
+  useEffect(() => {
+    const incrementView = async () => {
+        try{
+            
+let { data, error } = await supabase
+.rpc('increment', {
+  slug_text
+})
+if (error) console.error(error)
+else console.log(data)
+
+        }catch(error){
+            console.error("An error occurred while incrementing the view count: ", error)
+        }
+    }
+  }, [])
+
+  if (showCount) {
+    return <div>{views} views</div>;
+  } else {
+    return null;
+  }
+};
+
+export default ViewCounter;
