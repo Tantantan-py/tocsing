2249150e311b042bb0ea2a1f7c1659246f493f6b
Tantantan-py <exactbrycetan@gmail.com>
2024-08-07 16:13:41 -0500

    update insightRoll

--- src/app/(about)/layout.js
+++ src/app/(about)/layout.js
@@ -1,7 +1,18 @@
+import InsightRoll from "@/components/About/InsightRoll";
+
+const insights = [
+  "Years of Exp on tutoring CS/Non-CS background student to apply MSCS",
+  "Free guidance for hundreds of successful applicants",
+  "Provide long-term guidance services",
+  "To CS is not good as you see, but if you are already on this way, it may still worth a try for you",
+];
+
 export default function AboutLayout({ children }) {
   return (
     <main className="w-full flex flex-col items-center justify-between">
-      <div>insights</div>
+      <div>
+        <InsightRoll insights={insights} />
+      </div>
       {children}
     </main>
   );
