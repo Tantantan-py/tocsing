53aef11d8814687065dcd873ec8e5615384caded
Tantantan-py <exactbrycetan@gmail.com>
2024-08-09 14:28:18 -0500

    update site meta data

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,28 @@
+const siteMetadata = {
+  title: "Your Blog Title",
+  author: "Your Name",
+  headerTitle: "Your Header Title",
+  description: "A brief description of your blog",
+  language: "en-US",
+  theme: "system", // system, dark, or light
+  siteUrl: "https://www.yourwebsite.com",
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png",
+  email: "your-email@gmail.com",
+  github: "https://github.com/yourusername",
+  linkedin: "https://www.linkedin.com/in/yourusername",
+  locale: "en-US",
+  // Additional Metadata
+  twitter: "@yourtwitterhandle", // Twitter handle
+  facebookPage: "https://www.facebook.com/yourpage", // Facebook page URL
+  pinterest: "https://www.pinterest.com/yourprofile", // Pinterest profile URL
+  instagram: "https://www.instagram.com/yourprofile", // Instagram profile URL
+  rssFeed: "https://www.yourwebsite.com/rss.xml", // URL to your RSS feed
+  analyticsId: "UA-XXXXXXXXX-X", // Google Analytics or other tracking ID
+  favicon: "/favicon.ico", // URL to your favicon
+  themeColor: "#000000", // Theme color for mobile browsers
+  copyright: "© 2024 Your Name. All rights reserved.", // Copyright notice
+  contactFormUrl: "https://www.yourwebsite.com/contact", // URL to your contact form
+};
+
+module.exports = siteMetadata;
